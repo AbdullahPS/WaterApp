b30c5d9a6bf49612d0f0ab7be4f16c5640c10a6d
AbdullahPS <abdullahtabbal@github.com>
2021-06-17 21:11:41 +0300

    HomeScreen:Add Not responsible Textfields, Login Button, Register Button

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -4,6 +4,7 @@ import 'package:flutter_signin_button/flutter_signin_button.dart';
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
         appBar: AppBar(
           title: Text('WaterApp'),
@@ -11,17 +12,50 @@ class Homepage extends StatelessWidget {
         body: Container(
           color: Colors.transparent,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Padding(
-                padding: EdgeInsets.only(top: 40),
+                padding: EdgeInsets.only(top: 1),
                 child: Image.asset(
                   'assets/images/water.png',
                   width: MediaQuery.of(context).size.width,
                 ),
               ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Email",
+                ),
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Password"),
+              ),
+              TextButton(
+                  onPressed: () => null,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.redAccent),
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(60)),
+                    height: size.height * 0.075,
+                    width: size.width * 0.75,
+                    child: Center(
+                        child: Text(
+                      "Login",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    )),
+                  )),
               SignInButton(Buttons.Facebook, onPressed: () => null),
               SignInButton(Buttons.Google, onPressed: () => null),
+              GestureDetector(
+                child: Text(
+                  "Don't you have an account? Register here",
+                  style: TextStyle(color: Colors.blue),
+                ),
+              )
             ],
           ),
         ));
